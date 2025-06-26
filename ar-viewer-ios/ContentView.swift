9c8310f55149e158fe3bddf55babec2342d378bf
Bram Bogaerts <bram@superposition.cc>
2025-06-26 14:44:41 +0200

    Initial Commit

---
+++ ar-viewer-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ar-viewer-ios
+//
+//  Created by Bram Bogaerts on 26/06/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ar-viewer-ios/ar_viewer_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ar_viewer_iosApp.swift
+//  ar-viewer-ios
+//
+//  Created by Bram Bogaerts on 26/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ar_viewer_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
